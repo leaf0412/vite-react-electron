8e613a77b5532acf6243bbe938c90da8d48be740
yb <yb@yb.local>
2024-11-22 15:18:52 +0800

    feat: 实现窗口管理和文件协议调用

--- electron/main.ts
+++ electron/main.ts
@@ -1,66 +1,57 @@
-import { app, BrowserWindow } from 'electron'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
+import { app, BrowserWindow } from 'electron';
+import WindowManager from './handlers/window-manager';
+import { registerProtocol, unProtocol } from './handlers/create-protocol';
+import { isMac } from './config/constant';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+let winManager: WindowManager | null = null;
+let win: BrowserWindow | null = null;
 
-// The built directory structure
-//
-// ├─┬─┬ dist
-// │ │ └── index.html
-// │ │
-// │ ├─┬ dist-electron
-// │ │ ├── main.js
-// │ │ └── preload.mjs
-// │
-process.env.APP_ROOT = path.join(__dirname, '..')
+async function initApp() {
+  await app.whenReady();
 
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
-export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+  winManager = new WindowManager();
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+  winManager.initIpcHandlers();
 
-let win: BrowserWindow | null
+  win = winManager.createWindow({
+    show: false,
+  });
 
-function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
-    },
-  })
-
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
-
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
-  } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
-  }
+  win.on('ready-to-show', () => {
+    win?.show();
+  });
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
-})
+const gotTheLock = app.requestSingleInstanceLock();
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+if (!gotTheLock) {
+  app.quit();
+} else {
+  registerProtocol();
 
-app.whenReady().then(createWindow)
+  initApp().catch(console.error);
+
+  app.on('window-all-closed', () => {
+    win = null;
+    unProtocol();
+    winManager?.destroyIpcHandlers();
+    if (!isMac) {
+      app.quit();
+    }
+  });
+
+  app.on('second-instance', () => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
+      win.focus();
+    }
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      initApp().catch(console.error);
+    } else {
+      BrowserWindow.getAllWindows()[0].focus();
+    }
+  });
+}
